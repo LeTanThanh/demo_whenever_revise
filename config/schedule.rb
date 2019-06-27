f626a4c58e58fee06b5c29b4ea2cef3820995f10
LeTanThanh <tanthanhcr95@gmail.com>
2019-06-27 09:51:09 +0700

    fix_time_zone

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,8 +6,15 @@ set :output, "log/cron_job.log"
 # puts "Crontab is run on #{@environment}"
 # puts "Crontab is logged on #{@output}"
 
-every 1.minutes do
+def server_timezone time
+  Time.zone.parse(time).localtime
+end
+
+every :day, at: server_timezone("09:00 am") do
   command "echo 'example command'"
+end
+
+every 1.minutes do
   rake "user:create_random"
   runner "User.create_random"
 end
